030d619c20314f0f67bd7aa310b5b8726d7d1950
Richard Maples <reclmaples@gmail.com>
2022-05-24 08:32:52 -0400

    Update streamlit_app.py
    added an api call to fruity vice

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 streamlit.dataframe(fruits_to_show)
+
+#new section
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Watermelon")
+streamlit.text(fruityvice_response)
